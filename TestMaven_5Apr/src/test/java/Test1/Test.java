0c287a824f8cfe70a5323723d47261bb3ab803dd
vidya <vidyamangane@gmail.com>
2022-05-24 16:10:12 +0530

    This is commit from indian time zone

--- TestMaven_5Apr/src/test/java/Test1/Test.java
+++ TestMaven_5Apr/src/test/java/Test1/Test.java
@@ -9,12 +9,13 @@ public class Test {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\Software\\Drivers\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://www.google.com/");
 		Thread.sleep(3000);
 		driver.close();
+		System.out.println(" Browser closed");
 	}
 
 }
