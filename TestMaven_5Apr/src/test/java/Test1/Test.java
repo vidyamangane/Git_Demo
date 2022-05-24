2f19f90ed39e1cf7a6f49e2d6789507159a904d9
vidya <vidyamangane@gmail.com>
2022-05-24 15:37:40 +0530

    First commit

---
+++ TestMaven_5Apr/src/test/java/Test1/Test.java
@@ -0,0 +1,20 @@
+package Test1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.google.com/");
+		Thread.sleep(3000);
+		driver.close();
+	}
+
+}
